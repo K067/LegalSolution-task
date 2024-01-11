4e000b73c4b34b3c5e7a239f2eb81209d649e663
aismeg <iaskja20@gmai.com>
2024-01-11 21:52:35 +0100

    form ready

--- src/index.js
+++ src/index.js
@@ -1,19 +1,44 @@
 const dropDown = document.querySelector('.form-input-drop');
+const input = document.querySelectorAll('.form-input');
 const dropList = dropDown.querySelector('.country-list');
 const imgMod = dropDown.querySelector('.img-mod');
 const list = dropList.querySelectorAll('.country-name');
+const countryName = document.querySelector('.country-select');
+const formTitle = document.querySelector('.form-title');
+
+const defaultValue = () => {
+    dropList.classList.remove('active');
+    imgMod.style = "transform: none";
+    dropDown.style = "outline:none";
+}
 
 list.forEach(e => {
     e.addEventListener('click', e => {
-        document.querySelector('.country-select').textContent = e.target.textContent;
+        countryName.textContent = e.target.textContent;
     })
-})
+});
 
 dropDown.addEventListener('click', e => {
     const open = dropList.classList.toggle('active');
-    open ? imgMod.style = "transform: rotate(180deg)" : imgMod.style = "transform: none";
-})
-document.querySelector('.dropdown-group').addEventListener('blur', e => {
-    const close = dropList.classList.remove('active');
-    imgMod.style = "transform: none";
+    if (open) {
+        imgMod.style = "transform: rotate(180deg)";
+        dropDown.style = "outline: 1px solid #0E3B6C";
+
+    } else {
+        defaultValue();
+    }
 });
+
+document.querySelector('form').addEventListener('submit', e => {
+    e.preventDefault();
+
+    countryName.textContent === 'Country' ? dropDown.style = "outline: 2px solid red" :
+        input.forEach(e => {
+            e.value = '';
+        }) ? formTitle.textContent === "Join Us" : formTitle.textContent = "That's more like it";
+    countryName.textContent = 'Country';
+
+    setTimeout(() => {
+        formTitle.textContent = "Join Us";
+    }, 5000);
+})
