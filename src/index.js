2e90655174078d561f644729ceb4d01f42a6dd54
aismeg <iaskja20@gmai.com>
2024-01-11 18:31:15 +0100

    dropdown

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const dropDown = document.querySelector('.form-input-drop');
 const dropList = dropDown.querySelector('.country-list');
 const imgMod = dropDown.querySelector('.img-mod');
+const list = dropList.querySelectorAll('.country-name');
 
-// dropList.addEventListener('click', () => {
-//     console.log(dropList.selectedIndex);
-// });
+list.forEach(e => {
+    e.addEventListener('click', e => {
+        document.querySelector('.country-select').textContent = e.target.textContent;
+    })
+})
 
-dropDown.addEventListener('click', (e) => {
+dropDown.addEventListener('click', e => {
     const open = dropList.classList.toggle('active');
     open ? imgMod.style = "transform: rotate(180deg)" : imgMod.style = "transform: none";
+})
+document.querySelector('.dropdown-group').addEventListener('blur', e => {
+    const close = dropList.classList.remove('active');
+    imgMod.style = "transform: none";
 });
