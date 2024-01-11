6d0efd16a75958e9f490ba1877f1a07508b18c87
aismeg <iaskja20@gmai.com>
2024-01-11 17:16:35 +0100

    form config

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const dropDown = document.querySelector('.form-input-drop');
 const dropList = dropDown.querySelector('.country-list');
 const imgMod = dropDown.querySelector('.img-mod');
 
+// dropList.addEventListener('click', () => {
+//     console.log(dropList.selectedIndex);
+// });
+
 dropDown.addEventListener('click', (e) => {
     const open = dropList.classList.toggle('active');
     open ? imgMod.style = "transform: rotate(180deg)" : imgMod.style = "transform: none";
